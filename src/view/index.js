461bce31422ccd294b55a24106e7b6842d5d4d28
mercuriver <mercuriver@gmail.com>
2022-08-30 01:14:19 +0900

    refactor: view.js, 파일 구조 변경 및 filtersView 분리

--- src/view/index.js
+++ src/view/index.js
@@ -1,26 +1,17 @@
-const CLASS_NAME_SELETED = "selected";
-
 import todosView from "./todos.js";
 import counterView from "./counter.js";
+import filtersView from "./filters.js";
 
 const view = (targetElement, state) => {
-  const { currentFilter, todos } = state;
   const element = targetElement.cloneNode(true);
 
   const list = element.querySelector(".todo-list");
   const counter = element.querySelector(".todo-count");
   const filters = element.querySelector(".filters");
 
-  Array.from(filters.querySelectorAll("li a")).forEach((row) => {
-    if (row.textContent === currentFilter) {
-      row.classList.add(CLASS_NAME_SELETED);
-    } else {
-      row.classList.remove(CLASS_NAME_SELETED);
-    }
-  });
-
   list.replaceWith(todosView(list, state));
   counter.replaceWith(counterView(counter, state));
+  filters.replaceWith(filtersView(filters, state));
 
   return element;
 };
