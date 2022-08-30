2b4d39637b781da876dee6014ae97a5c5df0603d
mercuriver <mercuriver@gmail.com>
2022-08-31 00:12:31 +0900

    feat: web component 기반 todo app #4, Footer 웹 컴포넌트 작성

--- src/webComponents/components/Footer.js
+++ src/webComponents/components/Footer.js
@@ -1,3 +1,58 @@
-const view = () => {};
+const CLASS_NAME_SELETED = "selected";
 
-export default view;
+const getTodoCount = (todos) => {
+  const notCompleted = todos.filter((todo) => !todo.completed);
+
+  const { length } = notCompleted;
+
+  return length === 1 ? "1 Item left" : `${length} Items left`;
+};
+
+class Footer extends HTMLElement {
+  static observedAttributes() {
+    return ["filter", "todos"];
+  }
+
+  get todos() {
+    if (!this.hasAttribute("todos")) {
+      return [];
+    }
+
+    return JSON.parse(this.getAttribute("todos"));
+  }
+
+  set todos(value) {
+    this.setAttribute("todos", JSON.stringify(value));
+  }
+
+  get filter() {
+    return this.getAttribute("filter");
+  }
+
+  set filter(value) {
+    this.setAttribute("filter", value);
+  }
+
+  connectedCallback() {
+    const template = document.getElementById("footer");
+    const content = template.content.firstElementChild.cloneNode(true);
+
+    this.appendChild(content);
+
+    const { filter, todos } = this;
+
+    this.querySelectorAll("li a").forEach((row) => {
+      if (row.textContent === filter) {
+        row.classList.add(CLASS_NAME_SELETED);
+      } else {
+        row.classList.remove(CLASS_NAME_SELETED);
+      }
+    });
+
+    const label = getTodoCount(todos);
+
+    this.querySelector("span.todo-count").textContent = label;
+  }
+}
+
+export default Footer;
