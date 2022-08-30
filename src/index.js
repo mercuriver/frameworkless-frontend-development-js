5d02bf17d4034c1efcf8277fc7928c7b5794ebad
mercuriver <mercuriver@gmail.com>
2022-08-30 16:33:22 +0900

    feat: events 객체 생성

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import { appView, todosView, counterView, filtersView } from "./view/index.js";
 import { renderRoot, add } from "./resistry.js";
 import applyDiff from "./applyDiff.js";
 
-const state = {
-  todos: [],
-  currentFilter: "All",
-};
-
 // Todo: 'data-component' key 정리
 add("app", appView);
 add("todos", todosView);
 add("counter", counterView);
 add("filters", filtersView);
+
+const state = {
+  todos: [],
+  currentFilter: "All",
+};
+
+const events = {
+  deleteItem: (index) => {
+    state.todos.splice(index, 1);
+    render();
+  },
+  addItem: (text) => {
+    state.todos.push({
+      text,
+      completed: false,
+    });
+    render();
+  },
+};
 
 const render = () => {
   window.requestAnimationFrame(() => {
