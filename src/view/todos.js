7949955b54cac983abc557aef212f4151b48223b
mercuriver <mercuriver@gmail.com>
2022-08-30 16:37:54 +0900

    feat: todos view 내부, deleteItem 이벤트 연동 처리

--- src/view/todos.js
+++ src/view/todos.js
@@ -8,7 +8,7 @@ const createNewTodoNode = () => {
   return template.content.firstElementChild.cloneNode(true);
 };
 
-const getTodoElement = (todo) => {
+const getTodoElement = (todo, index, events) => {
   const { text, completed } = todo;
   const element = createNewTodoNode();
 
@@ -20,16 +20,21 @@ const getTodoElement = (todo) => {
     element.querySelector("input.toggle").checked = true;
   }
 
+  const handler = (e) => events.deleteItem(index);
+  element.querySelector("button.destroy").addEventListener("click", handler);
+
   return element;
 };
 
-const view = (targetElement, { todos }) => {
+const view = (targetElement, { todos }, events) => {
   const newTodoList = targetElement.cloneNode(true);
   newTodoList.innerHTML = "";
 
-  todos.map(getTodoElement).forEach((element) => {
-    newTodoList.appendChild(element);
-  });
+  todos
+    .map((todo, index) => getTodoElement(todo, index, events))
+    .forEach((element) => {
+      newTodoList.appendChild(element);
+    });
 
   return newTodoList;
 };
