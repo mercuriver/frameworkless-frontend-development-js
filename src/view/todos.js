d770126791cfa1f63fbe1c1a1d6fc2babfa16ae5
mercuriver <mercuriver@gmail.com>
2022-08-30 16:01:50 +0900

    refactor: 탬플릿 리터럴을 통한 dom 문자열 생성 코드, 실제 dom 사용으로 개선

--- src/view/todos.js
+++ src/view/todos.js
@@ -1,24 +1,37 @@
+let template;
+
+const createNewTodoNode = () => {
+  if (!template) {
+    template = document.getElementById("todo-item");
+  }
+
+  return template.content.firstElementChild.cloneNode(true);
+};
+
 const getTodoElement = (todo) => {
   const { text, completed } = todo;
+  const element = createNewTodoNode();
 
-  return `
-  <li ${completed ? 'class="completed"' : ""}>
-    <div class="view">
-      <input 
-        ${completed ? "checked" : ""}
-        class="toggle" 
-        type="checkbox">
-      <label>${text}</label>
-      <button class="destroy"></button>
-    </div>
-    <input class="edit" value="${text}">
-  </li>`;
+  element.querySelector("input.edit").value = text;
+  element.querySelector("label").textContent = text;
+
+  if (completed) {
+    element.classList.add("completed");
+    element.querySelector("input.toggle").checked = true;
+  }
+
+  return element;
 };
 
 const view = (targetElement, { todos }) => {
-  const newTodosList = targetElement.cloneNode(true);
-  newTodosList.innerHTML = todos.map(getTodoElement).join("");
-  return newTodosList;
+  const newTodoList = targetElement.cloneNode(true);
+  newTodoList.innerHTML = "";
+
+  todos.map(getTodoElement).forEach((element) => {
+    newTodoList.appendChild(element);
+  });
+
+  return newTodoList;
 };
 
 export default view;
