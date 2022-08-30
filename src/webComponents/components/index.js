c037b41484b746cacfd6fd1a4193362893169ef7
mercuriver <mercuriver@gmail.com>
2022-08-30 23:04:38 +0900

    feat: web component 기반 todo app #2, customElements 등록

---
+++ src/webComponents/components/index.js
@@ -0,0 +1,3 @@
+export { default as Application } from "./Application.js";
+export { default as Footer } from "./Footer.js";
+export { default as List } from "./List.js";
---
+++ src/webComponents/index.js
@@ -0,0 +1,5 @@
+import { Application, Footer, List } from "./components/index.js";
+
+window.customElements.define("todomvc-app", Application);
+window.customElements.define("todomvc-footer", Footer);
+window.customElements.define("todomvc-list", List);
