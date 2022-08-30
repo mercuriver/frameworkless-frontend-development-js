c62702eee5bfa85bab5f362d1e4427e9b6661396
mercuriver <mercuriver@gmail.com>
2022-08-30 09:24:26 +0900

    refactor: 구성 요소 기반 구조 변경 작업 #2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import getTodos from "./getTodos.js";
 import view from "./view/index.js";
 
+import todosView from "./view/todos.js";
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+
+// Todo: 'data-component' key 정리
+const registry = {
+  todos: todosView,
+  counter: counterView,
+  filters: filtersView,
+};
+
 const state = {
   todos: getTodos(),
   currentFilter: "All",
 };
 
 const main = document.querySelector(".todoapp");
+
+const renderWrapper = (component) => {
+  return (targetElement, state) => {
+    const element = component(targetElement, state);
+    const childComponents = element.querySelectorAll("[data-component]");
+
+    Array.from(childComponents).forEach((target) => {
+      const name = target.dataset.component;
+
+      const child = registry[name];
+      if (child) {
+        return;
+      }
+
+      target.replaceWith(child(target, state));
+    });
+
+    return element;
+  };
+};
 
 window.requestAnimationFrame(() => {
   const newMain = view(main, state);
