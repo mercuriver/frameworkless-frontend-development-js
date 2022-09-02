fa3901d50453dc33689e405908c1f63c50f423e8
mercuriver <mercuriver@gmail.com>
2022-09-02 16:57:16 +0900

    feat: ch07 상태 관리, 이벤트 버스 방식 #2

--- src/handleStateEventBus/model/model.js
+++ src/handleStateEventBus/model/model.js
@@ -1,101 +1,140 @@
-import observableFactory from "./observable.js";
+const cloneDeep = (x) => {
+  return JSON.parse(JSON.stringify(x));
+};
 
 const INITIAL_STATE = {
   todos: [],
   currentFilter: "All",
 };
 
-const modelFactory = (initialState = INITIAL_STATE) => {
-  const state = observableFactory(initialState);
+const addItem = (state, event) => {
+  const text = event.payload;
+  if (!text) {
+    return state;
+  }
 
-  const addItem = (text) => {
-    if (!text) {
-      return;
-    }
-
-    state.todos = [
+  return {
+    ...state,
+    todos: [
       ...state.todos,
       {
         text,
         completed: false,
       },
-    ];
+    ],
   };
+};
 
-  const updateItem = (index, text) => {
-    if (!text) {
-      return;
-    }
+const updateItem = (state, event) => {
+  const { text, index } = event.payload;
+  if (!text) {
+    return state;
+  }
 
-    if (index < 0) {
-      return;
-    }
+  if (index < 0) {
+    return state;
+  }
 
-    if (!state.todos[index]) {
-      return;
-    }
+  if (!state.todos[index]) {
+    return state;
+  }
 
-    state.todos = state.todos.map((todo, i) => {
+  return {
+    ...state,
+    todos: state.todos.map((todo, i) => {
       if (i === index) {
         todo.text = text;
       }
       return todo;
-    });
+    }),
   };
+};
 
-  const deleteItem = (index) => {
-    if (index < 0) {
-      return;
-    }
+const deleteItem = (state, event) => {
+  const index = event.payload;
+  if (index < 0) {
+    return state;
+  }
 
-    if (!state.todos[index]) {
-      return;
-    }
+  if (!state.todos[index]) {
+    return state;
+  }
 
-    state.todos = state.todos.filter((todo, i) => i !== index);
+  return {
+    ...state,
+    todos: state.todos.filter((todo, i) => i !== index),
   };
+};
 
-  const toggleItemCompleted = (index) => {
-    if (index < 0) {
-      return;
-    }
+const toggleItemCompleted = (state, event) => {
+  const index = event.payload;
 
-    if (!state.todos[index]) {
-      return;
-    }
+  if (index < 0) {
+    return state;
+  }
 
-    state.todos = state.todos.map((todo, i) => {
+  if (!state.todos[index]) {
+    return state;
+  }
+
+  return {
+    ...state,
+    todos: state.todos.map((todo, i) => {
       if (i === index) {
         todo.completed = !todo.completed;
       }
       return todo;
-    });
+    }),
   };
+};
 
-  const completeAll = () => {
-    state.todos = state.todos.map((todo, i) => {
+const completeAll = (state, event) => {
+  return {
+    ...state,
+    todos: state.todos.map((todo, i) => {
       todo.completed = true;
       return todo;
-    });
+    }),
   };
+};
 
-  const clearCompleted = () => {
-    state.todos = state.todos.filter((t) => !t.completed);
-  };
-
-  const changeFilter = (filter) => {
-    state.currentFilter = filter;
-  };
-
+const clearCompleted = (state, event) => {
   return {
-    addChangeListener: state.addChangeListener,
-    addItem,
-    updateItem,
-    deleteItem,
-    toggleItemCompleted,
-    completeAll,
-    clearCompleted,
-    changeFilter,
+    ...state,
+    todos: state.todos.filter((t) => !t.completed),
+  };
+};
+
+const changeFilter = (state, event) => {
+  return {
+    ...state,
+    currentFilter: event.payload,
+  };
+};
+
+const methods = {
+  ITEM_ADDED: addItem,
+  ITEM_UPDATED: updateItem,
+  ITEM_DELETED: deleteItem,
+  ITEMS_COMPLETED_TOGGLED: toggleItemCompleted,
+  ITEMS_MARKED_AS_COMPLETED: completeAll,
+  COMPLETED_ITEM_DELETED: clearCompleted,
+  FILTER_CHANGED: changeFilter,
+};
+
+const modelFactory = (initalState = INITIAL_STATE) => {
+  return (prevState, event) => {
+    if (!prevState) {
+      return cloneDeep(initalState);
+    }
+
+    const currentModifier = methods[event.type];
+
+    if (!currentModifier) {
+      return prevState;
+    }
+
+    return currentModifier(prevState, event);
   };
 };
 
